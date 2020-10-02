3d987baa56c9a257e0f853bb5267f9c164f0479f
Florian Overkamp <florian@overkamp.co>
2020-10-02 20:28:50 +0200

    rename running container name from test to asterisk just to be clear

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd build/
-docker stop test
-docker rm test
+docker stop asterisk
+docker rm asterisk
 docker build -t asterisk .
-docker run -d --rm --name=test \
+docker run -d --rm --name=asterisk \
   -p 5060:5060 \
   -v /home/florian/docker/asterisk/config:/etc/asterisk \
   asterisk
-docker exec -it test /bin/bash
+docker exec -it asterisk /bin/bash
 
